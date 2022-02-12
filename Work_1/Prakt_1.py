1cdc1fc563cc12f6e433c736acd6b016dd066466
Darina456777 <darina.minina.00.00@mail.ru>
2022-02-12 18:57:16 +0300

    Задание 1.1  решено (Практическая работа)

---
+++ Work_1/Prakt_1.py
@@ -0,0 +1,9 @@
+# Задание 1.1
+
+name1 = input('Введите имя:')
+name2 = input('Введите фамилию:')
+age = int(input('Введите возраст:'))
+
+information = (f"  имя: {name1}, фамилия: {name2}, возраст: {age}")
+
+print(information)
